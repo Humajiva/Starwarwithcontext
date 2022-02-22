5eeff3c68562ae5174d858bae9b4ed89d7fdb56f
Humajiva <humajiva123@gmail.com>
2022-02-22 02:05:28 +0000

    updates

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React ,{ useContext} from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
+			<li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+			  {store.favorites.map((fav , index) => {
+				  return<li key = {index}><a className="dropdown-item" href="#">{fav.name}</a></li>
+			  })}
+            {/* <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li> */}
+            </ul>
+        </li>
+
+				{/* <Link to="/demo">
 					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+				</Link> */}
 			</div>
 		</nav>
 	);
